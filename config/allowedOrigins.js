cbdc50eb4087c4ab44bf8b473347c3fe09d91131
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-11-25 15:50:32 -0300

    agrego allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,8 @@
 const allowedOrigins = [
     'https://front-app-final.netlify.app/',
+    'https://front-app-final.netlify.app/register',
+    'https://front-app-final.netlify.app/login',
+    'https://front-app-final.netlify.app/views',
     'http://localhost:3000'
 ];
 
