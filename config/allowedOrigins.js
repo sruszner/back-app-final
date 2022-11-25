5799479cbd21b48a64491ed9a1d11ff8ba83f6da
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-11-25 16:37:44 -0300

    Agrego validacion de token copiado

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'https://front-app-final.netlify.app/',
     'https://front-app-final.netlify.app/register',
     'https://front-app-final.netlify.app/login',
-    'https://front-app-final.netlify.app/views',
+    'https://front-app-final.netlify.app/auth',
     'http://localhost:3000'
 ];
 
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -6,15 +6,41 @@ const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
 
     const foundUser = await User.findOne({ refreshToken }).exec();
-    if (!foundUser) return res.sendStatus(403); //Forbidden 
+
+
+    if (!foundUser) {
+        jwt.verify(
+            refreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+            async (err, decoded) => {
+                if (err) return res.sendStatus(403); //Forbidden
+                // Delete refresh tokens of hacked user
+                const hackedUser = await User.findOne({ username: decoded.username }).exec();
+                hackedUser.refreshToken = [];
+                const result = await hackedUser.save();
+            }
+        )
+        return res.sendStatus(403); //Forbidden
+    }
+
+    const newRefreshTokenArray = foundUser.refreshToken.filter(rt => rt !== refreshToken);
+
     // evaluate jwt 
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
+        async (err, decoded) => {
+            if (err) {
+                // expired refresh token
+                foundUser.refreshToken = [...newRefreshTokenArray];
+                const result = await foundUser.save();
+            }
             if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+
+            // Refresh token was still valid
             const roles = Object.values(foundUser.roles);
             const accessToken = jwt.sign(
                 {
@@ -24,9 +50,22 @@ const handleRefreshToken = async (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '1h' }
+                { expiresIn: '10s' }
             );
-            res.json({ roles, accessToken })
+
+            const newRefreshToken = jwt.sign(
+                { "username": foundUser.username },
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: '15s' }
+            );
+            // Saving refreshToken with current user
+            foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+            const result = await foundUser.save();
+
+            // Creates Secure Cookie with refresh token
+            res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+
+            res.json({ accessToken })
         }
     );
 }
