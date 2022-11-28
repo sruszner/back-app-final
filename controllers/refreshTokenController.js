973d2a82cd0c5faa4f1df4d55b239b216f5b34e4
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-11-28 12:59:49 -0300

    Agrego samesite cookies

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -4,12 +4,10 @@ require('dotenv').config();
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
-
     if (!cookies?.jwt) return res.sendStatus(401);
-    
     const refreshToken = cookies.jwt;
-    const foundUser = await User.findOne({ refreshToken }).exec();
 
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) return res.sendStatus(403); //Forbidden 
     // evaluate jwt 
     jwt.verify(
