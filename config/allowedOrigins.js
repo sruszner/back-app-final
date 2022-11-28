20d66e8cea1cc6f6e5bbc8cd2a36e3d645361fcc
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-11-28 13:25:56 -0300

    Agrego headers CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'https://front-app-final.netlify.app/register',
     'https://front-app-final.netlify.app/login',
     'https://front-app-final.netlify.app/auth',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    '*'
 ];
 
 module.exports = allowedOrigins;
