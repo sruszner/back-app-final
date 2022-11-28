13ecce6c7496cb7c92d38e2a7e9cfeba19834203
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-11-28 16:51:52 -0300

    Agrego headers CORS

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,9 +3,10 @@ const allowedOrigins = require('../config/allowedOrigins');
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', "*");
         res.header('Access-Control-Allow-Credentials', true);
-/*      res.header('Access-Control-Allow-Methods', GET, POST);
-        res.header('Access-Control-Allow-Headers',  Content-Type, Authorization); */
+        res.header('Access-Control-Allow-Methods', "GET,HEAD,OPTIONS,POST,PUT");
+        res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorizatio");
     }
     next();
 }
