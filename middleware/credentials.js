9aee26a722f20ec85dd7420cfd7c43905bfc8dc0
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-12-01 15:41:59 -0300

    Config Cors

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -4,7 +4,7 @@ const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', "*");
-        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS");
         res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token");
         res.header('Access-Control-Max-Age', '86400');
