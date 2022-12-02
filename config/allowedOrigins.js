27589d1eb394a0cc5252392fc21dbcd2fd4b8d6d
Sebastian Ruszner <ruszner.sebastian@gmail.com>
2022-12-02 09:56:09 -0300

    Config Cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://front-app-final.netlify.app/',
+    'https://front-app-final.netlify.app',
     'https://front-app-final.netlify.app/register',
     'https://front-app-final.netlify.app/login',
     'https://front-app-final.netlify.app/auth',
